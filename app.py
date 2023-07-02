aee530df54be8b88c9071ebdbb7643665cdfb57d
AlvaroRGZ <alu0101362953@ull.edu.es>
2023-07-02 22:27:35 +0100

    Cleaning up app.py

--- app.py
+++ app.py
@@ -42,9 +42,9 @@ bootstrap = Bootstrap4(app)
 bbdd_host = 'localhost'
 def get_db_connection():
   conn = psycopg2.connect(host='localhost',
-        database = 'app',
-        user = 'admin',password = 'passwd',
-        port=5432)
+                          database = 'app',
+                          user = 'admin',password = 'passwd',
+                          port=5432)
   return conn
 
 def is_json(myjson):
@@ -248,20 +248,20 @@ def devicegraph(dev):
     min_max = cur.fetchall()
     if (min_max and min_max[0][0] is not None):
       fig.add_shape(type='line',
-                x0=df['rec_date'].min(),
-                x1=df['rec_date'].max(),
-                y0=min_max[0][0],
-                y1=min_max[0][0],
-                line=dict(color='red', dash='dash'),
-                name='min')
+                    x0=df['rec_date'].min(),
+                    x1=df['rec_date'].max(),
+                    y0=min_max[0][0],
+                    y1=min_max[0][0],
+                    line=dict(color='red', dash='dash'),
+                    name='min')
     if (min_max and min_max[0][1] is not None):
       fig.add_shape(type='line',
-                x0=df['rec_date'].min(),
-                x1=df['rec_date'].max(),
-                y0=min_max[0][1],
-                y1=min_max[0][1],
-                line=dict(color='green', dash='dash'),
-                name='max')
+                    x0=df['rec_date'].min(),
+                    x1=df['rec_date'].max(),
+                    y0=min_max[0][1],
+                    y1=min_max[0][1],
+                    line=dict(color='green', dash='dash'),
+                    name='max')
 
     fig.update_layout(showlegend=True)
     graphs.append(json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder))
@@ -297,8 +297,6 @@ def map():
 
   return render_template('map.html', devices_ = devices, gateways_ = gateways, map_ = fig, MB_TOKEN = mapbox_access_token)
 
-###
-
 @app.route('/modify/device/<eui>', methods=('GET', 'POST'))
 def modify_device(eui):
   if request.method == 'POST':
@@ -327,7 +325,6 @@ def modify_device(eui):
     # Get device information
     cur.execute('SELECT * FROM device WHERE eui=\'{}\';'.format(eui))
     device = cur.fetchall()
-    #dev = dict(dev)
     # From tuple to struct
     dev = {
       "eui": device[0][0],
@@ -338,7 +335,6 @@ def modify_device(eui):
       "description": device[0][5]
     }
 
-    print(dev)
     res = ""
     cur.close()
     conn.close()
@@ -374,7 +370,6 @@ def modify_gateway(eui):
     # Get gateway information
     cur.execute('SELECT * FROM gateway WHERE eui=\'{}\';'.format(eui))
     gateway = cur.fetchall()
-    #dev = dict(dev)
     # From tuple to struct
     gat = {
       "eui": gateway[0][0],
@@ -449,32 +444,32 @@ def limits(eui):
 
     parameter_names = []
     for key, value in dict1[0].items():
-        parameter_names.append(key)
+      parameter_names.append(key)
 
     form_items = list(request.form.items())
     for p in parameter_names:
-        name = request.form.get(f"{p}_name")
-        mind = request.form.get(f"{p}_new_min")
-        maxd = request.form.get(f"{p}_new_max")
-        print(name, mind, maxd)
-        print("------------------------")
+      name = request.form.get(f"{p}_name")
+      mind = request.form.get(f"{p}_new_min")
+      maxd = request.form.get(f"{p}_new_max")
+      print(name, mind, maxd)
+      print("------------------------")
 
-        if mind or maxd:
-            # Validate and convert to decimal
-            mind_decimal = decimal.Decimal(mind) if mind else None
-            maxd_decimal = decimal.Decimal(maxd) if maxd else None
-            # Check if the record already exists
-            cur.execute("SELECT COUNT(*) FROM device_limits WHERE eui = %s AND parameter = %s;", (eui, p))
-            count = cur.fetchone()[0]
+      if mind or maxd:
+        # Validate and convert to decimal
+        mind_decimal = decimal.Decimal(mind) if mind else None
+        maxd_decimal = decimal.Decimal(maxd) if maxd else None
+        # Check if the record already exists
+        cur.execute("SELECT COUNT(*) FROM device_limits WHERE eui = %s AND parameter = %s;", (eui, p))
+        count = cur.fetchone()[0]
 
-            if count > 0:
-                # Update the existing record
-                cur.execute("UPDATE device_limits SET min = %s, max = %s WHERE eui = %s AND parameter = %s;",
-                            (mind or None, maxd or None, eui, p))
-            else:
-                # Insert a new record
-                cur.execute("INSERT INTO device_limits (eui, parameter, min, max) VALUES (%s, %s, %s, %s);",
-                            (eui, p, mind or None, maxd or None))
+        if count > 0:
+          # Update the existing record
+          cur.execute("UPDATE device_limits SET min = %s, max = %s WHERE eui = %s AND parameter = %s;",
+                      (mind or None, maxd or None, eui, p))
+        else:
+          # Insert a new record
+          cur.execute("INSERT INTO device_limits (eui, parameter, min, max) VALUES (%s, %s, %s, %s);",
+                      (eui, p, mind or None, maxd or None))
 
     conn.commit()
     cur.close()
