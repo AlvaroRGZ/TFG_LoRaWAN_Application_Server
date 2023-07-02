a1b9a03438f9d284c2454a86ffa4a13aa3ca7913
AlvaroRGZ <alu0101362953@ull.edu.es>
2023-07-02 12:58:29 +0100

    Added alert mechanism

--- app.py
+++ app.py
@@ -23,6 +23,11 @@ mapbox_access_token = open(".mapbox_token").read()
 
 import requests
 
+
+# Enviar alertas por correo electronico
+import smtplib
+from email.message import EmailMessage
+
 #import grpc
 #from chirpstack_api.as_pb.external import api
 
@@ -457,3 +462,24 @@ def call():
   req = api.GetDeviceRequest(eui, metadata=auth_token)
   print(req)
   return render_template("call.html", dev_info_ = resp)
+
+
+#def send_alert(dest, message, value):
+#  # Open the plain text file whose name is in textfile for reading.
+#  with open(textfile) as fp:
+#      # Create a text/plain message
+#      msg = EmailMessage()
+#      msg.set_content(fp.read())
+#
+#  # me == the sender's email address
+#  # you == the recipient's email address
+#  msg['Subject'] = f'The contents of {textfile}'
+#  msg['From'] = me
+#  msg['To'] = you
+#
+#  # Send the message via our own SMTP server.
+#  s = smtplib.SMTP('localhost')
+#  s.send_message(msg)
+#  s.quit()
+#  print(req)
+#  return render_template("call.html", dev_info_ = resp)
